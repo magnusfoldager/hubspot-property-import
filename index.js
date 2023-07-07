52224b3372d206dfaab48a7762fbc810da2545d8
Magnus Foldager <m_foldager@hotmail.com>
2023-07-07 13:32:05 +0200

    Add more types

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 require('dotenv').config()
+const util = require('util')
 
 const csv = require('csv-parser')
 const fs = require('fs')
 const hubspot = require('@hubspot/api-client');
 
-const hubspotClient = new hubspot.Client({ "accessToken": process.env.ACCESS_TOKEN });
+const hubspotClient = new hubspot.Client({ "accessToken": process.env.PROD_ACCESS_TOKEN });
 
 const objectType = 'company'
+const fileName = 'input-file.csv'
 
 let inputs = []
 
 const addProperty = (row) => {
   if (row.fieldType == "number") return addNumber(row)
   if (row.fieldType == "select") return addSelect(row)
+  if (row.fieldType == "string") return addString(row)
+  if (row.fieldType == "multi") return addMulti(row)
+  if (row.fieldType == "owner") return addOwner(row)
+  if (row.fieldType == "date") return addDate(row)
   console.log(`Unsupported Field Type: "${row.fieldType}"`)
 }
 
 const addNumber = (row) => {
-  console.log(`Adding Select "${row.label}"`)
+  console.log(`Adding Number "${row.label}"`)
 
   inputs.push(
     {
@@ -32,9 +38,9 @@ const addNumber = (row) => {
 }
 
 const addSelect = (row) => {
-  console.log(`Adding Number "${row.label}"`)
+  console.log(`Adding Select "${row.label}"`)
 
-  console.log(row.options)
+  const fixedOptions = row.options.replace(/FALSE/g,'false')
 
   inputs.push(
     {
@@ -44,13 +50,75 @@ const addSelect = (row) => {
       "type": "enumeration",
       "fieldType": "select",
       "groupName": row.groupName,
-      "options": JSON.parse(row.options)
+      "options": JSON.parse(fixedOptions)
+    }
+  )
+}
+
+const addString = (row) => {
+  console.log(`Adding String "${row.label}"`)
+
+  inputs.push(
+    {
+      "name": row.name,
+      "label": row.label,
+      "description": row.description,
+      "type": "string",
+      "fieldType": "text",
+      "groupName": row.groupName
+    }
+  )
+}
+
+const addMulti = (row) => {
+  console.log(`Adding Multi "${row.label}"`)
+
+  inputs.push(
+    {
+      "name": row.name,
+      "label": row.label,
+      "description": row.description,
+      "type": "string",
+      "fieldType": "textarea",
+      "groupName": row.groupName
+    }
+  )
+}
+
+const addDate = (row) => {
+  console.log(`Adding Date "${row.label}"`)
+
+  inputs.push(
+    {
+      "name": row.name,
+      "label": row.label,
+      "description": row.description,
+      "type": "date",
+      "fieldType": "date",
+      "groupName": row.groupName
+    }
+  )
+}
+
+const addOwner = (row) => {
+  console.log(`Adding Owner "${row.label}"`)
+
+  inputs.push(
+    {
+      "name": row.name,
+      "label": row.label,
+      "description": row.description,
+      "type": "enumeration",
+      "fieldType": "select",
+      "groupName": row.groupName,
+      "externalOptions": true,
+      "referencedObjectType": "OWNER"
     }
   )
 }
 
 
-fs.createReadStream('input.csv')
+fs.createReadStream(fileName)
   .pipe(csv())
   .on('data', (row) => addProperty(row))
   .on('end', () => {
